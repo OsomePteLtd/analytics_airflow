ff34a4d24822234307dbd83a67b65260b6a2649a
Alexander Prokhorov <aleksandr.prokhorov@osome.com>
2022-11-25 19:23:00 +0400

    first dag to test sync between gh and gcs

---
+++ dags/test_dag.py
@@ -0,0 +1,26 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'test_sync_dag',
+    default_args=default_args,
+    description='just a dag to test that GH sync works',
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=20))
+
+# priority_weight has type int in Airflow DB, uses the maximum.
+t1 = BashOperator(
+    task_id='echo',
+    bash_command='echo test',
+    dag=dag,
+    depends_on_past=False,
+    priority_weight=2**31 - 1,
+    do_xcom_push=False)
