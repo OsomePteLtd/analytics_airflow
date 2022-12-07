ca2b258e3bd08a8b42ea526904ab90d3921e2f79
Alexander Prokhorov <aleksandr.prokhorov@osome.com>
2022-12-07 16:49:19 +0300

    init jumpcloud dag

---
+++ src/dags/etl/jumpcloud.py
@@ -0,0 +1,83 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+
+import logging
+import pendulum
+from datetime import timedelta
+
+from utils.config import SYNCED_AT_FIELD
+from utils.utils import task_fail_slack_alert
+from utils.hooks.jumpcloud_hook import JumpcloudHook
+
+DATASET_ID = 'jumpcloud'
+SYSTEMS_TABLE_NAME = 'systems_test'
+
+default_args = {
+    'start_date': pendulum.datetime(2022, 12, 6, tz="UTC"),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'on-failure-callback': task_fail_slack_alert
+}
+
+jumpcloud_dag = DAG(
+    dag_id='jumpcloud',
+    default_args=default_args,
+    description='Integration with jumpcloud',
+    schedule_interval='0 2 * * *',
+    dagrun_timeout=timedelta(minutes=20))
+
+
+def upload_systems(**kwargs):
+    # initializing hooks
+    jumpcloud_hook = JumpcloudHook()
+    bigquery_hook = BigQueryHook(use_legacy_sql=False)
+    logging.info(f'Initialized hooks')
+
+    # extracting data from jumpcloud
+    list_of_systems = jumpcloud_hook.get_full_systems_list()
+    logging.info(f'Extracted data from jumpcloud')
+
+    # prepare
+    data_column_name = 'system_json'
+    current_date = kwargs.get('data_interval_start'),
+    list_of_systems = [{"json": {data_column_name: i, SYNCED_AT_FIELD: str(current_date)}} for i in list_of_systems]
+    logging.info(f'Transformed data and inserted a timestamp')
+
+    # check and create bq table
+    # -must have partitions by SYNCED_AT_FIELD
+    if not bigquery_hook.table_exists(dataset_id=DATASET_ID, table_id=SYSTEMS_TABLE_NAME):
+        logging.info(f'Table wasn\'t found, creating one')
+        bigquery_hook.create_empty_table(
+            project_id=DATASET_ID,
+            table_id=SYSTEMS_TABLE_NAME,
+            schema_field=[
+                {"name": data_column_name, "type": "JSON", "mode": "REQUIRED"},
+                {"name": SYNCED_AT_FIELD, "type": "TIMESTAMP", "mode": "REQUIRED"},
+            ],
+            time_partitioning={
+                "type": 'DAY',
+                "field": SYNCED_AT_FIELD,
+            }
+        )
+        logging.info(f'Table was created')
+
+    logging.info(f'Uploading rows to BigQuery')
+
+    # upload rows
+    bigquery_hook.insert_all(
+        dataset_id=DATASET_ID,
+        table_id=SYSTEMS_TABLE_NAME,
+        rows=list_of_systems
+    )
+
+    logging.info(f'Done successfully')
+
+
+with jumpcloud_dag as dag:
+    systems = PythonOperator(
+        task_id='systems',
+        provide_context=True,
+        python_callable=upload_systems)
+
+    systems
