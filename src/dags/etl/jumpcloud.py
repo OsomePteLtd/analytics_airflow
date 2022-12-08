f473f3a972ef58a7ad15b5c309b845c4e1aaee3a
Alexander Prokhorov <aleksandr.prokhorov@osome.com>
2022-12-08 22:15:48 +0300

    added system users task

--- src/dags/etl/jumpcloud.py
+++ src/dags/etl/jumpcloud.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 
@@ -13,6 +14,7 @@ from utils.hooks.jumpcloud_hook import JumpcloudHook
 
 DATASET_ID = 'jumpcloud'
 SYSTEMS_TABLE_NAME = 'systems'
+SYSTEM_USERS_TABLE_NAME = 'system_users'
 
 default_args = {
     'start_date': pendulum.datetime(2022, 12, 6, tz="UTC"),
@@ -37,7 +39,6 @@ def upload_systems(**kwargs):
 
     # extracting data from jumpcloud
     extracted_systems = jumpcloud_hook.get_full_systems_list()
-    logging.info(f'Extracted data from jumpcloud')
     logging.info(f'Extracted data from jumpcloud')
 
     # prepare
@@ -87,10 +88,75 @@ def upload_systems(**kwargs):
     logging.info(f'Done successfully')
 
 
+def upload_system_users(**kwargs):
+    # initializing hooks
+    jumpcloud_hook = JumpcloudHook()
+    bigquery_hook = BigQueryHook(use_legacy_sql=False)
+    logging.info(f'Initialized hooks')
+
+    # extracting data from jumpcloud
+    extracted_system_users = jumpcloud_hook.get_full_system_users_list()
+    logging.info(f'Extracted data from jumpcloud')
+
+    # prepare
+    data_column_name = 'system_users_json'
+    current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    logging.info(f'Current date is - {current_date}')
+
+    prepared_rows = []
+    for system_user in extracted_system_users:
+        prepared_row = {
+            data_column_name: json.dumps(system_user),
+            SYNCED_AT_FIELD: current_date
+        }
+
+        prepared_rows.append(prepared_row)
+
+    logging.info(f'Transformed data and inserted a timestamp')
+
+    # check and create bq table
+    # -must have partitions by SYNCED_AT_FIELD
+    if not bigquery_hook.table_exists(dataset_id=DATASET_ID, table_id=SYSTEM_USERS_TABLE_NAME):
+        logging.info(f'Table wasn\'t found, creating one')
+        bigquery_hook.create_empty_table(
+            dataset_id=DATASET_ID,
+            table_id=SYSTEM_USERS_TABLE_NAME,
+            schema_fields=[
+                {"name": data_column_name, "type": "JSON", "mode": "REQUIRED"},
+                {"name": SYNCED_AT_FIELD, "type": "TIMESTAMP", "mode": "REQUIRED"},
+            ],
+            time_partitioning={
+                "type": 'DAY',
+                "field": SYNCED_AT_FIELD,
+            },
+            cluster_fields=[SYNCED_AT_FIELD]
+        )
+        logging.info(f'Table was created')
+
+    logging.info(f'Uploading rows to BigQuery')
+
+    # upload rows
+    bigquery_hook.insert_all(
+        dataset_id=DATASET_ID,
+        table_id=SYSTEM_USERS_TABLE_NAME,
+        rows=prepared_rows
+    )
+
+    logging.info(f'Done successfully')
+
+
 with jumpcloud_dag as dag:
+
+    init = DummyOperator(task_id='init')
+
     systems = PythonOperator(
         task_id='systems',
         provide_context=True,
         python_callable=upload_systems)
 
-    systems
+    system_users = PythonOperator(
+        task_id='system_users',
+        provide_context=True,
+        python_callable=upload_system_users)
+
+    init >> [systems, system_users]
--- src/dags/utils/hooks/jumpcloud_hook.py
+++ src/dags/utils/hooks/jumpcloud_hook.py
@@ -78,6 +78,62 @@ class JumpcloudHook(BaseHook):
 
         return response
 
+    def system_users_request(self,
+                             skip: int = 0,
+                             limit: int = 10,
+                             fields: Optional[Union[str, None]] = None,
+                             search: Optional[Union[str, None]] = None,
+                             sort: Optional[Union[str, None]] = None,
+                             filter_val: Optional[Union[str, None]] = None
+                             ) -> requests.models.Response:
+        """
+        This endpoint returns all system users
+
+        Method docs:
+        https://docs.jumpcloud.com/api/1.0/index.html#tag/Systemusers
+
+
+        :param limit: The number of records to return at once. Limited to 100.
+        :param skip: The offset into the records to return, should be >= 0
+        :param fields: Use a space seperated string of field parameters to include the data in the response. If omitted,
+         the default list of fields will be returned
+        :param search: A nested object containing a searchTerm string or array of strings and a list of fields to search
+         on.
+        :param sort: Use space separated sort parameters to sort the collection. Default sort is ascending. Prefix with
+        - to sort descending.
+        :param filter_val: A filter to apply to the query. See the supported operators below. For more complex searches,
+         see the related /search/<domain> endpoints, e.g. /search/systems.
+         Read more in API docs.
+        :return: Response contains dict with keys ['totalCount', 'results']
+
+        """
+
+        systems_endpoint = '/system_users'
+
+        params = {
+            "limit": str(limit),
+            "skip": str(skip),
+        }
+
+        if fields:
+            params['fields'] = fields
+
+        if search:
+            params['search'] = search
+
+        if sort:
+            params['sort'] = sort
+
+        if filter_val:
+            params['filter'] = filter_val
+
+        logging.info(f'Requesting a system users with following parameters {params}')
+        response = requests.get(url=self.host + systems_endpoint, headers=self._auth_headers, params=params)
+
+        logging.info(f'Status code: {response.status_code}, returning a response object')
+
+        return response
+
     def get_full_systems_list(self) -> list:
 
         full_result_list = []
@@ -86,6 +142,37 @@ class JumpcloudHook(BaseHook):
 
         while skip_val != total_count:
             response = self.systems_request(
+                skip=skip_val,
+                limit=100)
+
+            if not response.ok:
+                logging.error(f'Something is wrong with the request - {response.reason}')
+                raise requests.exceptions.RequestException(f'Status code {response.status_code}')
+
+            response_json = response.json()
+
+            total_count = response_json['totalCount']
+
+            results_list = response_json['results']
+            results_list_len = len(results_list)
+
+            full_result_list.extend(results_list)
+            logging.info(f'Extracted {results_list_len} records, total length is {len(full_result_list)}')
+
+            skip_val += results_list_len
+
+        logging.info('Extracting is done')
+
+        return full_result_list
+
+    def get_full_system_users_list(self) -> list:
+
+        full_result_list = []
+        total_count = -1
+        skip_val = 0
+
+        while skip_val != total_count:
+            response = self.system_users_request(
                 skip=skip_val,
                 limit=100)
 
